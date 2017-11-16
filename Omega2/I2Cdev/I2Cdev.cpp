d7133b634ec5c04cb0ea355ea4b031d74f3dec94
Jonny Dark <jonny@jonnydark.com>
2017-11-16 16:34:10 +0000

    Added Omega2 device

---
+++ Omega2/I2Cdev/I2Cdev.cpp
@@ -0,0 +1,211 @@
+// I2Cdev library collection - Main I2C device class
+// Abstracts bit and byte I2C R/W functions into a convenient class
+// RaspberryPi bcm2835 library port: bcm2835 library available at http://www.airspayce.com/mikem/bcm2835/index.html
+// Based on Arduino's I2Cdev by Jeff Rowberg <jeff@rowberg.net>
+//
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+#include "I2Cdev.h"
+#include <stdio.h>
+#include <onion-i2c.h>
+
+I2Cdev::I2Cdev() { }
+
+char sendBuf[256];
+char recvBuf[256];
+
+/** Read a single bit from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitNum Bit position to read (0-7)
+ * @param data Container for single bit value
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t *data) {
+    int b;
+    uint8_t status = i2c_readByte(I2C_DEFAULT_ADAPTER, devAddr, regAddr, &b);
+    *data = (uint8_t)b & (1 << bitNum);
+    return status;
+}
+
+
+/** Read a single bit from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitNum Bit position to read (0-15)
+ * @param data Container for single bit value
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16_t *data) {
+    uint8_t b[2];
+    uint8_t status = i2c_read(I2C_DEFAULT_ADAPTER, devAddr, regAddr, b, 2);
+    uint16_t bCombined = (b[0] << 8) | b[1];
+    *data = bCombined & (1 << bitNum);
+    return status;
+}
+
+/** Read multiple bits from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitStart First bit position to read (0-7)
+ * @param length Number of bits to read (not more than 8)
+ * @param data Container for right-aligned value (i.e. '101' read from any bitStart position will equal 0x05)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t *data) {
+    uint8_t status;
+    int byte;
+    if ((status = i2c_readByte(I2C_DEFAULT_ADAPTER, devAddr, regAddr, &byte)) != 0) {
+        uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+        uint8_t b = byte;
+        b &= mask;
+        b >>= (bitStart - length + 1);
+        *data = b;
+    }
+    return status;
+}
+
+/** Read single byte from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param data Container for byte value read from device
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readByte(uint8_t devAddr, uint8_t regAddr, uint8_t *data) {
+    int b;
+    int status = i2c_readByte(I2C_DEFAULT_ADAPTER, devAddr, regAddr, &b);
+    *data = (uint8_t)b;
+    return status;
+}
+
+/** Read multiple bytes from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr First register regAddr to read from
+ * @param length Number of bytes to read
+ * @param data Buffer to store read data in
+ * @return I2C_TransferReturn_TypeDef http://downloads.energymicro.com/documentation/doxygen/group__I2C.html
+ */
+int8_t I2Cdev::readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data) {
+    return i2c_read(I2C_DEFAULT_ADAPTER, devAddr, regAddr, data, length);
+}
+
+/** write a single bit in an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitNum Bit position to write (0-7)
+ * @param value New bit value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data) {
+    int byte;
+    i2c_readByte(I2C_DEFAULT_ADAPTER, devAddr, regAddr, &byte);
+    uint8_t b = (uint8_t)byte;
+    b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
+    return i2c_write(I2C_DEFAULT_ADAPTER, devAddr, regAddr, b);
+}
+
+/** Write multiple bits in an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitStart First bit position to write (0-7)
+ * @param length Number of bits to write (not more than 8)
+ * @param data Right-aligned value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
+  //      010 value to write
+  // 76543210 bit numbers
+  //    xxx   args: bitStart=4, length=3
+  // 00011100 mask byte
+  // 10101111 original value (sample)
+  // 10100011 original & ~mask
+  // 10101011 masked | value
+    int byte;
+    if (i2c_readByte(I2C_DEFAULT_ADAPTER, devAddr, regAddr, &byte) != 0) {
+        uint8_t b = byte;
+        uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+        data <<= (bitStart - length + 1); // shift data into correct position
+        data &= mask; // zero all non-important bits in data
+        b &= ~(mask); // zero all important bits in existing byte
+        b |= data; // combine data with existing byte
+        return i2c_write(I2C_DEFAULT_ADAPTER, devAddr, regAddr, b);
+    } else {
+        return false;
+    }
+}
+
+/** Write single byte to an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register address to write to
+ * @param data New byte value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t data) {
+    return i2c_write(I2C_DEFAULT_ADAPTER, devAddr, regAddr, (int)data);
+}
+
+/** Read single word from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param data Container for word value read from device
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readWord(uint8_t devAddr, uint8_t regAddr, uint16_t *data) {
+    return readWords(devAddr, regAddr, 1, data);
+}
+
+/** Read multiple words from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr First register regAddr to read from
+ * @param length Number of words to read
+ * @param data Buffer to store read data in
+ * @return Number of words read (-1 indicates failure)
+ */
+int8_t I2Cdev::readWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t *data) {
+    uint8_t b[I2C_BUFFER_SIZE];
+    uint8_t status = i2c_read(I2C_DEFAULT_ADAPTER, devAddr, regAddr, b, length*2);
+    for (uint8_t i = 0; i < length; i++) {
+        data[i] = (b[i*2] << 8) | b[i*2 + 1];
+    }
+    return status;
+}
+
+bool I2Cdev::writeWord(uint8_t devAddr, uint8_t regAddr, uint16_t data){
+    return writeWords(devAddr, regAddr, 1, &data);
+}
+
+bool I2Cdev::writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data){
+    return i2c_writeBuffer(I2C_DEFAULT_ADAPTER, devAddr, regAddr, data, length);
+}
+
+bool I2Cdev::writeWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t *data){
+    uint8_t buffer[I2C_BUFFER_SIZE];
+    for (uint8_t i = 0; i < length; i++) {
+        buffer[i*2] = (uint8_t) (data[i] >> 8); // MSByte
+        buffer[i*2 + 1] = (uint8_t) (data[i] >> 0); // LSByte
+    }
+    return i2c_writeBuffer(I2C_DEFAULT_ADAPTER, devAddr, regAddr, buffer, length);
+}
